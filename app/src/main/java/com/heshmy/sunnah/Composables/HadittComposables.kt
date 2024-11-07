1ceeabf8f83388b2332d0b424dff70a0b476264a
essam <essamheshmey>
2024-11-07 14:06:54 +0200

    Create Haditt widgit

---
+++ app/src/main/java/com/heshmy/sunnah/Composables/HadittComposables.kt
@@ -0,0 +1,62 @@
+package com.heshmy.sunnah.Composables
+
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.heshmy.domain.models.HadithItem
+import com.heshmy.sunnah.ui.theme.PurpleGrey40
+
+
+var hadith: HadithItem = HadithItem(
+    100010,
+    "bukhari",
+    1,
+    0,
+    1.00,
+    1,
+    "كتاب بدء الوحى ",
+    "باب كَيْفَ كَانَ بَدْءُ الْوَحْىِ إِلَى رَسُولِ اللَّهِ صلى الله عليه وسلم",
+    "حَدَّثَنَا عَبْدُ اللَّهِ بْنُ يُوسُفَ، قَالَ أَخْبَرَنَا مَالِكٌ، عَنْ هِشَامِ بْنِ عُرْوَةَ، عَنْ أَبِيهِ، عَنْ عَائِشَةَ أُمِّ الْمُؤْمِنِينَ ـ رضى الله عنها ـ أَنَّ الْحَارِثَ بْنَ هِشَامٍ ـ رضى الله عنه ـ سَأَلَ رَسُولَ اللَّهِ صلى الله عليه وسلم فَقَالَ يَا رَسُولَ اللَّهِ كَيْفَ يَأْتِيكَ الْوَحْىُ فَقَالَ رَسُولُ اللَّهِ صلى الله عليه وسلم ‏\"‏ أَحْيَانًا يَأْتِينِي مِثْلَ صَلْصَلَةِ الْجَرَسِ ـ وَهُوَ أَشَدُّهُ عَلَىَّ ـ فَيُفْصَمُ عَنِّي وَقَدْ وَعَيْتُ عَنْهُ مَا قَالَ، وَأَحْيَانًا يَتَمَثَّلُ لِيَ الْمَلَكُ رَجُلاً فَيُكَلِّمُنِي فَأَعِي مَا يَقُولُ ‏\"‏‏.‏ قَالَتْ عَائِشَةُ رضى الله عنها وَلَقَدْ رَأَيْتُهُ يَنْزِلُ عَلَيْهِ الْوَحْىُ فِي الْيَوْمِ الشَّدِيدِ الْبَرْدِ، فَيَفْصِمُ عَنْهُ وَإِنَّ جَبِينَهُ لَيَتَفَصَّدُ عَرَقًا‏.‏",
+    0,
+    1.0,
+    0,
+    1,
+    1.0,
+    ""
+    )
+
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun preview(){
+    hadittItem(hadith)
+}
+@Composable
+fun hadittItem(item:HadithItem){
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Gray)
+            .padding(8.dp)
+
+    ){
+        Text(text = "${item.collection}")
+        Text(text = "${item.bookName}")
+        Text(text = "${item.babName}")
+
+        Text(text = "${item.hadithText}")
+    }
+
+
+}
---
+++ app/src/main/java/com/heshmy/sunnah/scrines/HadittList.kt
@@ -0,0 +1,16 @@
+package com.heshmy.sunnah.scrines
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun listScreenPreview() {
+    listScreen()
+}
+
+@Composable
+fun listScreen() {
+Text(text = "essam")
+}
